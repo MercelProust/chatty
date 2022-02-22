2723ab3e5efd880bcea8bce73233f86f23414533
Mercel <790977345@qq.com>
2022-02-22 09:40:39 +0800

    chatty主入口文件初始添加

---
+++ chatty.erl
@@ -0,0 +1,20 @@
+%% ---
+%%  chatty局域网聊天工具的主入口模块函数
+%%---
+
+-module(chatty).
+-export([chatty_main/0]).
+
+chatty_main() -> 
+    load_config(),
+    start_chatty_server(1069),
+    start_chatty_client().
+
+load_config() ->
+    io:format("In load config function~n").
+
+start_chatty_server(Port) ->
+    io:format("In start chatty server with port:~B~n", [Port]).
+
+start_chatty_client() ->
+    io:format("In start chatty client function~n").
